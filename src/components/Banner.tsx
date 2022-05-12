cff7649dbb83c0fccb9fea389beeb60e234321c8
Splash-07 <59502884+Splash-07@users.noreply.github.com>
2022-05-12 20:07:57 +0300

    Transition to React. Adding UI

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ import { Flex } from "@chakra-ui/react";
 const Banner = () => {
   return (
     <Flex alignItems="center" backgroundColor="#5D6AA2" padding="2" border="1px solid blue.900" boxShadow="sm">
-      Version 1.3.0
+      Version 2.0.0
     </Flex>
   );
 };
